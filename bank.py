879e4f18763ccb4b70e51a5757d0f34beab6a8b7
Abhijit <abhiCoder123>
2019-03-10 00:07:10 +0530

    first commit

---
+++ bank.py
@@ -0,0 +1,110 @@
+# Importing the Libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from scipy import integrate
+
+# Importing the dataset
+dataset=pd.read_csv('bank-additional-full.csv',delimiter=";")
+X=dataset.iloc[:,:-1].values
+y=dataset.iloc[:,20].values
+
+#Encoding categorical data
+
+#Label Encoding of Categorical Data present in Matrix of Features
+from sklearn.preprocessing import LabelEncoder
+labelencoder_X=LabelEncoder()
+for i in range(9):
+  X[:,i+1]=labelencoder_X.fit_transform(X[:,i+1])
+X[:,14]=labelencoder_X.fit_transform(X[:,14])
+
+#Label Encoding of output variable 
+labelencoder_Y=LabelEncoder()
+y=labelencoder_Y.fit_transform(y)
+
+#One Hot Encoding of categorical Data present in matrix of features
+from sklearn.preprocessing import OneHotEncoder
+onehotencoder= OneHotEncoder(categorical_features=[1,2,3,4,5,6,7,8,9,14])
+X=onehotencoder.fit_transform(X).toarray()
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+# Fitting Random Forest Classification to the Training set
+from sklearn.ensemble import RandomForestClassifier
+classifier = RandomForestClassifier(n_estimators = 35, criterion = 'entropy', random_state = 0)
+classifier.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+
+
+
+# CAP Curve Function
+def capcurve(y_values, y_preds_proba):
+    num_pos_obs = np.sum(y_values)
+    num_count = len(y_values)
+    rate_pos_obs = float(num_pos_obs) / float(num_count)
+    ideal = pd.DataFrame({'x':[0,rate_pos_obs,1],'y':[0,1,1]})
+    xx = np.arange(num_count) / float(num_count - 1)
+    
+    y_cap = np.c_[y_values,y_preds_proba]
+    y_cap_df_s = pd.DataFrame(data=y_cap)
+    y_cap_df_s = y_cap_df_s.sort_values([1], ascending=False).reset_index(level = y_cap_df_s.index.names, drop=True)
+    
+    print(y_cap_df_s.head(20))
+    
+    yy = np.cumsum(y_cap_df_s[0]) / float(num_pos_obs)
+    yy = np.append([0], yy[0:num_count-1]) #add the first curve point (0,0) : for xx=0 we have yy=0
+    
+    percent = 0.5
+    row_index = int(np.trunc(num_count * percent))
+    
+    val_y1 = yy[row_index]
+    val_y2 = yy[row_index+1]
+    if val_y1 == val_y2:
+        val = val_y1*1.0
+    else:
+        val_x1 = xx[row_index]
+        val_x2 = xx[row_index+1]
+        val = val_y1 + ((val_x2 - percent)/(val_x2 - val_x1))*(val_y2 - val_y1)
+    
+    sigma_ideal = 1 * xx[num_pos_obs - 1 ] / 2 + (xx[num_count - 1] - xx[num_pos_obs]) * 1
+    sigma_model = integrate.simps(yy,xx)
+    sigma_random = integrate.simps(xx,xx)
+    
+    ar_value = (sigma_model - sigma_random) / (sigma_ideal - sigma_random)
+    
+    fig, ax = plt.subplots(nrows = 1, ncols = 1)
+    ax.plot(ideal['x'],ideal['y'], color='grey', label='Perfect Model')
+    ax.plot(xx,yy, color='red', label='User Model')
+    ax.plot(xx,xx, color='blue', label='Random Model')
+    ax.plot([percent, percent], [0.0, val], color='green', linestyle='--', linewidth=1)
+    ax.plot([0, percent], [val, val], color='green', linestyle='--', linewidth=1, label=str(val*100)+'% of positive obs at '+str(percent*100)+'%')
+    
+    plt.xlim(0, 1.02)
+    plt.ylim(0, 1.25)
+    plt.title("CAP Curve - a_r value ="+str(ar_value))
+    plt.xlabel('% of the data')
+    plt.ylabel('% of positive obs')
+    plt.legend()
+    plt.show()
+    
+
+#Then we create the CAP Curve with the following code :
+
+y_pred_proba = classifier.predict_proba(X_test)
+capcurve(y_values=y_test, y_preds_proba=y_pred_proba[:,1])
+
